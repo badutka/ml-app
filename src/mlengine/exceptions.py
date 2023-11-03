a6e62da530dc6b03b54154bcc030665d5c2fdcb8
Bartosz Dutka <dutkabar@gmail.com>
2023-11-03 23:00:40 +0100

    renamed components to library name (mlengine); added exceptions and logger

---
+++ src/mlengine/exceptions.py
@@ -0,0 +1,43 @@
+import sys
+from logger import logger
+
+
+class ExceptionWithIDMixin(object):
+    def __init__(self, id: int | str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = id
+
+
+class ExceptionWithMsgMixin(object):
+    def __init__(self, message: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+
+class GenericException(ExceptionWithIDMixin, ExceptionWithMsgMixin, Exception):
+    def __init__(self, id: int | str, message: str):
+        super().__init__(id, message)
+
+
+class DetailedGenericException(GenericException):
+    def __init__(self, id: int | str, message: str, error_detail: sys, *args, **kwargs):
+        # todo: if e hasattr id or message -> get them from e into new exception
+        _, _, exc_tb = error_detail.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        error_info = f"\n\tError occured in {file_name} on line number {exc_tb.tb_lineno}."
+        if message:
+            error_info += f"\n\t\t Error message: \n\t\t\t{message}."
+
+        super().__init__(id, error_info)
+
+    def __str__(self):
+        return self.message
+
+
+class DetailedGenericExceptionLogged(DetailedGenericException):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        logger.exception(self.message)
+
+    def __str__(self):
+        return self.message
