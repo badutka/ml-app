32aedc6416cffa093c996ec3dc12200ad670bd6d
Bartosz Dutka <dutkabar@gmail.com>
2023-11-05 00:03:12 +0100

    added pipeline flow and data ingestion pipeline

---
+++ src/mlengine/pipelines/pipeline.py
@@ -0,0 +1,55 @@
+import abc
+
+from mlengine.config.settings import settings
+from mlengine.common.logger import logger
+from mlengine.data_read.read import DataIngestion
+
+
+class Pipeline(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'run') and
+                callable(subclass.run) or
+                NotImplemented)
+
+    @staticmethod
+    @abc.abstractmethod
+    def run() -> None:
+        """Run the particular pipeline"""
+        raise NotImplementedError
+
+
+class PipelineRunner:
+    def __init__(self, pipeline_object: Pipeline, stage_name: str):
+        self.pipeline_object = pipeline_object
+        self.stage_name = stage_name
+        self.__stage_marker = 5 * '='
+        self.__separator = 30 * '*'
+
+    def run_pipeline(self):
+        try:
+            logger.info(f"{self.__stage_marker} {self.stage_name} started {self.__stage_marker}")
+            self.pipeline_object.run()
+            logger.info(f"{self.__stage_marker} {self.stage_name} completed {self.__stage_marker}\n{self.__separator}")
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+
+class DataIngestionPipeline(Pipeline):
+    @staticmethod
+    def run():
+        data_ingestion = DataIngestion(config=settings.data_ingestion)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+def run_pipeline(option: str):
+    match option:
+        case 'data_ingestion':
+            pipeline = DataIngestionPipeline()
+        case other:
+            raise ValueError(f'Incorrect option: {other}.')
+
+    runner = PipelineRunner(pipeline_object=pipeline, stage_name="Data Ingestion Stage")
+    runner.run_pipeline()
