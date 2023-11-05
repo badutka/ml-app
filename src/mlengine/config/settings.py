91c26d34a47e3a52fb68dd671d13445a756e940f
Bartosz Dutka <dutkabar@gmail.com>
2023-11-05 23:42:32 +0100

    updated settings classes

--- src/mlengine/config/settings.py
+++ src/mlengine/config/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from dataclasses import dataclass
 from pathlib import Path
 import typing
@@ -19,6 +19,39 @@ class Singleton(type):
         return cls._instances[cls]
 
 
+@dataclass(frozen=True)
+class DataIngestionSettings(BaseModel):
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataValidationSettings(BaseModel):
+    data_file: Path
+    root_dir: Path
+    status_file: Path
+
+
+@dataclass(frozen=True)
+class PlotLayoutsSettings(BaseModel):
+    features_plots_layout: typing.Dict = Field(default_factory=dict)
+
+    def __init__(self, **data):
+        if 'features_plots_layout' in data and isinstance(data['features_plots_layout'], ConfigBox):
+            data['features_plots_layout'] = dict(data['features_plots_layout'])
+        super().__init__(**data)
+
+
+@dataclass(frozen=True)
+class Settings(BaseModel):
+    artifacts_root: Path
+    data_ingestion: DataIngestionSettings
+    data_validation: DataValidationSettings
+    plot_layouts: PlotLayoutsSettings
+
+
 class SettingsManager(metaclass=Singleton):
     def __init__(self):
         pass
@@ -29,21 +62,9 @@ class SettingsManager(metaclass=Singleton):
     # def get_settings(self) -> ConfigBox:
     #     return self.settings
 
-    def initialize_settings(self, settings_class) -> ConfigBox:
-        return settings_class(**self.settings)
-
-
-class DataIngestionSettings(BaseModel):
-    root_dir: str
-    source_URL: str
-    local_data_file: str
-    unzip_dir: str
-
-
-class Settings(BaseModel):
-    artifacts_root: str
-    data_ingestion: DataIngestionSettings
+    def initialize_settings(self) -> Settings:
+        return Settings(**self.settings)
 
 
 # settings = SettingsManager().get_settings()
-settings = SettingsManager().initialize_settings(Settings)
+settings = SettingsManager().initialize_settings()
